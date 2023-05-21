a3f14fa65618edf5fc8c6646c280e47f74d16b1e
wengchonc <wengchonc@student.unimelb.edu.au>
2023-05-21 23:18:09 +1000

    del failed test function

--- script.js
+++
@@ -1,5 +0,0 @@
-function bannerChange(){
-    if document.getElementsByClassName("poster-img")="poster_1_image.png" {
-        document.getElementsByClassName("poster-img")="poster_1_image.png"
-    }
-}
