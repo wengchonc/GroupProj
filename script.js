a50454d320122a05595175480825421b20600d49
wengchonc <wengchonc@student.unimelb.edu.au>
2023-05-21 23:17:41 +1000

    added hover on src bar and made poster scroll

---
+++ script.js
@@ -0,0 +1,5 @@
+function bannerChange(){
+    if document.getElementsByClassName("poster-img")="poster_1_image.png" {
+        document.getElementsByClassName("poster-img")="poster_1_image.png"
+    }
+}
