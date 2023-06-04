8b1f252c1d5924e8724316460894cd596cc8201d
eihannngunimelb <e.hann123@gmail.com>
2023-06-04 14:10:21 +1000

    added function for the number next to cart

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-
+function updateCartQuantity() {
+    const quantityInput = document.querySelector(".quantity-input");
+    const cartQuantity = document.querySelector(".cart-quantity");
+    const quantity = parseInt(quantityInput.value);
+    
+    if (cartQuantity) {
+      cartQuantity.textContent = quantity > 0 ? quantity : "";
+    } else {
+      const cartIcon = document.querySelector("#cart-icon");
+      const newCartQuantity = document.createElement("span");
+      newCartQuantity.classList.add("cart-quantity");
+      newCartQuantity.textContent = quantity > 0 ? quantity : "";
+      cartIcon.parentNode.insertBefore(newCartQuantity, cartIcon.nextSibling);
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    const addToCartButton = document.querySelector(".add-to-cart-button");
+    addToCartButton.addEventListener("click", function() {
+      updateCartQuantity();
+    });
+  });
+  
